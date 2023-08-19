4a878ed865c1f1e3c8db7eae1f63c19d49a09b0c
Sanura <sanurajerome@gmail.com>
2023-08-19 10:03:11 -0500

    Initial Commit

---
+++ FirstIoSapp/FirstIoSappApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstIoSappApp.swift
+//  FirstIoSapp
+//
+//  Created by sanura jerome on 8/19/23.
+//
+
+import SwiftUI
+
+@main
+struct FirstIoSappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
