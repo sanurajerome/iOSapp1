f533c33ec80b92b19643a8090bf43af8419cedb7
Sanura <sanurajerome@gmail.com>
2023-08-19 10:35:34 -0500

    EmojiApp

--- FirstIoSapp/ContentView.swift
+++ FirstIoSapp/ContentView.swift
@@ -7,15 +7,32 @@
 
 import SwiftUI
 
+enum Emoji: String, CaseIterable{
+    case 😀,🤣,😍
+    
+}
 struct ContentView: View {
+    
+    @State var selection: Emoji = .😀
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Testing")
+        NavigationView{
+            
+            VStack{
+                Text(selection.rawValue)
+                    .font(.system(size:150))
+                
+                Picker("Select Emoji",selection: $selection){
+                    ForEach(Emoji.allCases, id: \.self){emoji in
+                        Text(emoji.rawValue)
+                    }
+                }
+                
+                .pickerStyle(.segmented)
+            }
+            .navigationTitle("Emoji Lovers!")
+            .padding()
         }
-        .padding()
     }
 }
 
